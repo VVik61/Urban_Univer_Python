ba225350ede4482cdfdeaa7c59c6b40b4e6d481c
VVik61 <vikivvoron@Gmail.com>
2024-10-29 08:36:53 +0300

    включить 2 файла в коммит

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # Первая программа, выводит Hello world
 
-print('Hello world')  # Press Ctrl+F8 to toggle the breakpoint.
+print('Hello world')  # Press Ctrl+F8 to toggle the breakpoint.  нажать Ctrl+F8 для установки брекпоинта
