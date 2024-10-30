b49f1de1f25551a7a1239c64f4e49e295d346784
VVik61 <vikivvoron@Gmail.com>
2024-10-30 09:40:41 +0300

    домашка к теме Переменные

---
+++ dz_peremenie.py
@@ -0,0 +1,6 @@
+count_dom_zadan = 12
+count_hour = 1.5
+name_curs = 'Python-Разработчик'
+print('Курс: ' + name_curs + ', всего задач:' + str(count_dom_zadan)\
+    +', затрачено часов: ' + str(count_hour) + ', среднее время выполнения '\
+    + str(count_hour / count_dom_zadan) +' часа')
